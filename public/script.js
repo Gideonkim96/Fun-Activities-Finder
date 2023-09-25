1756687b9356f3e81e2d8ea10b9af6de28914c4b
Caroline <110534527+Ckimatu@users.noreply.github.com>
2023-09-25 13:09:01 +0000

    implemented the uncustomised generator

--- public/script.js
+++ public/script.js
@@ -15,3 +15,30 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+// Function to fetch a random activity from the Bored API
+async function fetchRandomActivity() {
+  try {
+    const response = await fetch('https://www.boredapi.com/api/activity?type=recreational');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data.activity;
+  } catch (error) {
+    console.error('Error fetching random activity:', error);
+    return 'Failed to fetch activity. Please try again later.';
+  }
+}
+
+// Function to update the activity on the webpage
+async function updateActivityOnPage() {
+  const randomActivity = await fetchRandomActivity();
+  const activityElement = document.getElementById('activity');
+  activityElement.textContent = randomActivity;
+}
+
+// Event listener for the button click
+const randomBtn = document.getElementById('randomBtn');
+randomBtn.addEventListener('click', updateActivityOnPage);
+
