9334a905536dd11c31c065e960bb9177d8c458c4
Caroline <110534527+Ckimatu@users.noreply.github.com>
2023-09-25 06:57:41 +0000

    added directory

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Use the provided port or default to 3000
 
+// Serve static files (index.html, CSS, JavaScript, etc.) from the 'public' directory
+app.use(express.static('public'));
+
 // Define a route for the homepage
 app.get('/', (req, res) => {
   res.send('Welcome to the Fun Activities Website!');
