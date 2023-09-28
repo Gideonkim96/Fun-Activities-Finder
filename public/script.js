67754f6ef1d960e1f5b6ac0feedcedb0a282b994
Caroline <110534527+Ckimatu@users.noreply.github.com>
2023-09-28 08:20:56 +0000

    update filter

--- public/script.js
+++ public/script.js
@@ -16,35 +16,54 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-async function fetchRandomActivity(price) {
-  try {
-    // Construct the API URL with filters
-    const apiUrl = `https://www.boredapi.com/api/activity?type=recreational&relaxation&price=${price}`;
 
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    return data.activity;
+// Function to fetch a random activity from the Bored API with price range
+async function fetchRandomActivity(priceRange) {
+  try {
+      const response = await fetch(`https://www.boredapi.com/api/activity?type=recreational&type=relaxation&${priceRange}`);
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data.activity;
   } catch (error) {
-    console.error('Error fetching random activity:', error);
-    return 'Failed to fetch activity. Please try again later.';
+      console.error('Error fetching random activity:', error);
+      return 'Failed to fetch activity. Please try again later.';
   }
 }
 
-// Function to update the activity on the webpage with filters
+// Function to update the activity on the webpage
 async function updateActivityOnPage() {
-  // Get selected filter values 
-  const selectedPrice = document.getElementById('price').value
+  // Get the selected price range from the dropdown
+  const priceDropdown = document.getElementById('price');
+  const selectedPrice = priceDropdown.value;
 
-  // Fetch a random activity with filters
-  const randomActivity = await fetchRandomActivity(selectedPrice);
+  // Define the price range based on the selected option
+  let priceRange = '';
+  switch (selectedPrice) {
+      case '0':
+          // Free
+          priceRange = 'price=0';
+          break;
+      case '0.2':
+          // Cheap
+          priceRange = 'minprice=0.1&maxprice=0.2';
+          break;
+      case '0.3':
+          // Expensive
+          priceRange = 'minprice=0.3';
+          break;
+      default:
+          // Default to Free
+          priceRange = 'price=0';
+          break;
+  }
 
+  const randomActivity = await fetchRandomActivity(priceRange);
   const activityElement = document.getElementById('activity');
   activityElement.textContent = randomActivity;
 }
 
-// Event listener for the "Generate Random Activity" button
+// Event listener for the button click
 const randomBtn = document.getElementById('randomBtn');
 randomBtn.addEventListener('click', updateActivityOnPage);
